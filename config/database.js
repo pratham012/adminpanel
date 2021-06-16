e2c4de38ee699b2f2e83bc6c5136e81d3378d5d2
pratham012 <pvijayvargiya@tibicle.com>
2021-06-16 20:19:37 +0530

    commit

--- config/database.js
+++ config/database.js
@@ -1,12 +1,11 @@
 const database = {
     client: 'mysql2',
     connection: {
-        host: 'adminpanel--deploy.herokuapp.com',
+        host: 'proxy',
+        path:"https://adminpanel--deploy.herokuapp.com",
         user: 'root',
         password: '1234',
-        database: 'admin_panel',
-        port : 3306,
-        min : 0
+        database: 'admin_panel'
     }
 }
 
