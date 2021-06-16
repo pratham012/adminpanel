5ff38b90c14325ae5fb8566c452ec04a32efc5e4
pratham012 <pvijayvargiya@tibicle.com>
2021-06-16 19:23:45 +0530

    commit

--- config/database.js
+++ config/database.js
@@ -1,11 +1,12 @@
 const database = {
     client: 'mysql2',
+    ssl: { rejectUnauthorized: false },
     connection: {
         host: 'adminpanel--deploy.herokuapp.com',
         user: 'root',
         password: '1234',
         database: 'admin_panel',
-        ssl: { rejectUnauthorized: false }
+        
     }
 }
 
